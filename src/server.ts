57142d041649e2e1750e1545150dbbfb51061257
Kevin J <kjoseph1910@hotmail.com>
2026-02-06 10:09:53 +0100

    feat: Add new server structure

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,22 @@
 import express, { Express } from 'express'
 import router from './router'
-import db from './config/db'
-import colors from 'colors'
+
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, { swaggerUiOptions } from './config/swagger'
 import cors, { CorsOptions } from 'cors'
 import morgan from 'morgan'
 
 
-export async function connectDB() {
-    try {
-        await db.authenticate()
-        db.sync()
-        // console.log(colors.blue('connection success'))
-    } catch (error) {
-        // console.log(error)
-        console.log(colors.red('connection error'))
-    }
-}
-connectDB()
-
 const server: Express = express()
 
 const corsOptions: CorsOptions = {
     origin: function (origin, callback) {
-        if (!origin || origin === process.env.FRONTEND_URL) {
-            callback(null, true)
+        const whitelist = [process.env.FRONTEND_URL];
+
+        if (!origin || whitelist.includes(origin)) {
+            callback(null, true);
         } else {
-            callback(new Error("conexion no permitida"))
+            callback(new Error("conexion no permitida"));
         }
     }
 }
